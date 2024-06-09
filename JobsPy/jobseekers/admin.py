bba8cbb0754e5df46d428f7d1eb87eefe5d83294
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-06-09 14:28:01 +0300

    Add Admin

--- JobsPy/jobseekers/admin.py
+++ JobsPy/jobseekers/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import JobSeeker, Education, Experience
 
-# Register your models here.
+
+@admin.register(JobSeeker)
+class JobSeekerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'city', 'nationality', 'occupation', 'activated')
+    list_filter = ('city', 'nationality', 'occupation', 'activated')
+    search_fields = ('user__username', 'first_name', 'last_name', 'city', 'nationality', 'occupation')
+    readonly_fields = ('user',)
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('job_seeker', 'institution', 'start_date', 'end_date')
+    list_filter = ('institution', 'start_date', 'end_date')
+    search_fields = ('job_seeker__user__username', 'institution', 'description')
+    readonly_fields = ['job_seeker']
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('job_seeker', 'company', 'start_date', 'end_date')
+    list_filter = ('company', 'start_date', 'end_date')
+    search_fields = ('job_seeker__user__username', 'company', 'description')
+    readonly_fields = ['job_seeker']
