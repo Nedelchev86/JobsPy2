06cfe49ff054c0659b54a5357b79204647892198
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-06-09 09:59:34 +0300

    Add Accounts app

--- JobsPy/settings.py
+++ JobsPy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'JobsPy.accounts'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,22 @@ WSGI_APPLICATION = 'JobsPy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "re-database",
+        'USER': "yfzwywiqln",
+        'PASSWORD': "Aq6jdzVb$igFQP7u",
+        'HOST': "re-server.postgres.database.azure.com",
+        'PORT': '5432',  # Port is 5432 by default
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
