bffba6cc78b7bbc0113379b810df78c4e622180d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-07-24 20:56:16 +0300

    Add add and Edit Education

--- JobsPy/jobseekers/serializers.py
+++ JobsPy/jobseekers/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import JobSeeker
+from .models import JobSeeker, Education
 from ..jobs.models import Skills, Applicant
 
 
@@ -20,3 +20,9 @@ class ApplicantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Applicant
         fields = '__all__'  # Include all fields or specify the fields you need
+
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = ['id', 'image', 'institution', 'description', 'start_date', 'end_date']
