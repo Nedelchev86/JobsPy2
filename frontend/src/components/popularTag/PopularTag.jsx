3fcf971f166719b13f46e14652c6802057a8bf0a
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-08-04 17:44:51 +0300

    Loading in popular tags

--- frontend/src/components/popularTag/PopularTag.jsx
+++ frontend/src/components/popularTag/PopularTag.jsx
@@ -2,24 +2,29 @@ import useFetch from "../../hooks/useFetch";
 import {Link, NavLink} from "react-router-dom";
 import styles from "./PopularTag.module.css";
 import {useLocation} from "react-router-dom";
+import Loading from "../loading/Loading";
 
 export default function PopularTags({skill, handleInputChange}) {
-    const {data: skills, error, isLoading, refetch} = useFetch(`${import.meta.env.VITE_API_URL}skills/`, []);
+    const {data: skills, error, loading, refetch} = useFetch(`${import.meta.env.VITE_API_URL}skills/`, []);
     const {search} = useLocation();
     const currentSkill = new URLSearchParams(search).get("skill");
 
     return (
         <div className="widget popular-tag-widget">
-            <div className="tags">
-                <NavLink className={({isActive}) => (isActive && !currentSkill ? styles.darkBlue : "")} to={`/jobs/`}>
-                    All jobs
-                </NavLink>
-                {skills.map((s) => (
-                    <NavLink className={({isActive}) => (isActive && currentSkill === s.name ? styles.darkBlue : "")} key={s.id} to={`/jobs/?skill=${s.name}`}>
-                        {s.name}
+            {loading ? (
+                <Loading />
+            ) : (
+                <div className="tags">
+                    <NavLink className={({isActive}) => (isActive && !currentSkill ? styles.darkBlue : "")} to={`/jobs/`}>
+                        All jobs
                     </NavLink>
-                ))}
-            </div>
+                    {skills.map((s) => (
+                        <NavLink className={({isActive}) => (isActive && currentSkill === s.name ? styles.darkBlue : "")} key={s.id} to={`/jobs/?skill=${s.name}`}>
+                            {s.name}
+                        </NavLink>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
