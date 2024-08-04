56fc5409125a1c9581bc3846b30ffe6042810914
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-08-04 13:02:40 +0300

    Check something

---
+++ frontend/src/api/companyApi.js
@@ -0,0 +1,21 @@
+import useFetch from "../hooks/useFetch";
+import useFetchWithToken from "../hooks/useFetchWithToken";
+import {API_URL} from "../config";
+
+const API_BASE_URL = `${API_URL}company/`;
+
+export function getAllCompanies() {
+    return useFetch(`${API_BASE_URL}`, []);
+}
+
+export function getCompanyById(id) {
+    return useFetch(`${API_BASE_URL}${id}`, {});
+}
+
+export function getApplicantsList() {
+    return useFetchWithToken(`${API_BASE_URL}applicants/`, []);
+}
+
+export function getJobsByCompany() {
+    return useFetchWithToken(`${API_BASE_URL}created-jobs/`, []);
+}
