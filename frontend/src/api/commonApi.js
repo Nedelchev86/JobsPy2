c598c949e58f6df5c146514ad3c99150fdbc6343
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-08-06 06:55:30 +0300

    Add more service endpoints

---
+++ frontend/src/api/commonApi.js
@@ -0,0 +1,13 @@
+import usePut from "../hooks/usePut";
+import {API_URL} from "../config";
+import useFetch from "../hooks/useFetch";
+
+const API_BASE_URL = API_URL;
+
+export function changePassword() {
+    return usePut(`${API_BASE_URL}user/change-password/`, {});
+}
+
+export function getAllSkills() {
+    return useFetch(`${API_BASE_URL}skills/`, []);
+}
--- frontend/src/api/companyApi.js
+++ frontend/src/api/companyApi.js
@@ -1,4 +1,5 @@
 import useFetch from "../hooks/useFetch";
+import usePut from "../hooks/usePut";
 import useFetchWithToken from "../hooks/useFetchWithToken";
 import {API_URL} from "../config";
 
@@ -18,4 +19,12 @@ export function getApplicantsList() {
 
 export function getJobsByCompany() {
     return useFetchWithToken(`${API_BASE_URL}created-jobs/`, []);
+}
+
+export function getCompanyForEdit() {
+    return useFetchWithToken(`${API_BASE_URL}update/`);
+}
+
+export function putCompanyUpdate(data) {
+    return usePut(`${API_BASE_URL}update/`);
 }
