7b072841feb63370eff0ed58d9b263c2dbc330e9
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-08-06 20:10:31 +0300

    Add notification if user is not activated

--- frontend/src/components/dashboardMenu/MenuLayout.jsx
+++ frontend/src/components/dashboardMenu/MenuLayout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 import JobSeekerMenu from "./JobseekerMenu";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import {useAuth} from "../../contexts/authContexts";
 import CompanyMenu from "./CompanyMenu";
+import styles from "./MenuLayout.module.css";
 
 const MenuLayout = () => {
     const {user} = useAuth();
@@ -17,6 +18,27 @@ const MenuLayout = () => {
             />
             <div className="bookmarked section">
                 <div className="container">
+                    {user?.user_type === "company" && !user.user.activated && (
+                        <div className={styles["alert-users"]} role="alert">
+                            Please{" "}
+                            <Link to="/dashboard/edit" className="alert-link">
+                                {" "}
+                                complete your profile
+                            </Link>{" "}
+                            to get access to create and edit job offers .
+                        </div>
+                    )}
+
+                    {user?.user_type === "jobseeker" && !user.user.activated && (
+                        <div className={styles["alert-users"]} role="alert">
+                            Please{" "}
+                            <Link to="/dashboard/edit" className="alert-link">
+                                {" "}
+                                complete your profile
+                            </Link>{" "}
+                            to get access to apply for job.
+                        </div>
+                    )}
                     <div className="alerts-inner">
                         <div className="row">
                             {user && user.user_type === "jobseeker" && <JobSeekerMenu />}
